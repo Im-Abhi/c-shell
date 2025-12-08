5941db1025c602e8b9cf62f6f3161ea70e3b324d
Abhishek Gupta <86161191+Im-Abhi@users.noreply.github.com>
2025-12-08 16:39:08 +0530

    Tokenisation
    use glibc tokenization method

--- main.cpp
+++ main.cpp
@@ -1,12 +1,28 @@
 #include <stdio.h>
 #include <string>
+#include <cstring>
 
 #include "./lib/linenoise.h"
 
 #define PROMPT "$ "
 #define HISTORY_LENGTH 1024
+#define MAX_ARGS 64
+#define TOKEN_SEP " \t"
 
 using namespace std;
+
+// tokenization method from glibc
+int s_read(char *input, char **args, int max_args) {
+    int i = 0;
+    char *token = strtok(input, TOKEN_SEP);
+    while (token != NULL && i < (max_args - 1)) {
+        args[i++] = token;
+        token = strtok(NULL, TOKEN_SEP);
+    }
+
+    args[i] = NULL;
+    return i;
+}
 
 int main(void) {
     if(!linenoiseHistorySetMaxLen(HISTORY_LENGTH)) {
@@ -15,9 +31,28 @@ int main(void) {
     }
 
     char *line;
+    char *args[MAX_ARGS];
 
     while((line = linenoise(PROMPT)) != NULL) {
-        fprintf(stdout, "%s\n", line);
+
+        // read step
+        int args_count =  s_read(line, args, MAX_ARGS);
+
+        fprintf(stdout, "Read %d args\n", args_count);
+
+        for(int i = 0; i < args_count; i++) {
+            fprintf(stdout, "arg[%d]: %s\n", i, args[i]);
+        }
+
+        // skip empty lines
+        if (args_count == 0) {
+            linenoiseFree(line);
+            continue;
+        }
+
+        // TODO: eval + processing step
+
+
         linenoiseHistoryAdd(line);
         linenoiseFree(line);
     }
